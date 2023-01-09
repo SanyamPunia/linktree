83781c82cafe71c715f9f8afd32a220019c64203
SanyamPunia <lewarends@gmail.com>
2023-01-10 01:01:57 +0530

    linktree

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="text-white">
+      <body>{children}</body>
+    </html>
+  );
+}
